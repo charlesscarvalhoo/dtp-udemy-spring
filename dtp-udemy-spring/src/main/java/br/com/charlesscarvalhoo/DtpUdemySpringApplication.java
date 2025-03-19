bc85f23374e04accd78ebf0c34c2e72cf09cdc76
Charles Carvalho <charles.carvalho@dataprev.gov.br>
2025-03-19 16:43:05 -0300

    inicio curso spring boot

---
+++ dtp-udemy-spring/src/main/java/br/com/charlesscarvalhoo/DtpUdemySpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.charlesscarvalhoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtpUdemySpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DtpUdemySpringApplication.class, args);
+	}
+
+}
---
+++ dtp-udemy-spring/src/test/java/br/com/charlesscarvalhoo/DtpUdemySpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.charlesscarvalhoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DtpUdemySpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
